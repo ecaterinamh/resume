fc25513a1419a6ff8d26826e54c7064a4dacdfbe
Nomad <ecaterinamihai97@gmail.com>
2023-11-25 10:17:59 +0200

    'third commit'

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -17,7 +17,7 @@ export default function Contact(){
                         <Col xs={12} md={6} xl={6} className='contact-content'>
                                 <div className='personal-contact'>
                                     <h2 style={{textAlign: 'center'}}>Thank you for visiting my website! </h2>
-                                    <h5 style={{textAlign: 'left'}}>I would like to connect with you on LinkedIn or GitHub. I'm interested in any new opportunity to work in the web development industry. 
+                                    <h5 style={{textAlign: 'justify'}}>I would like to connect with you on LinkedIn or GitHub. I'm interested in any new opportunity to work in the web development industry. 
 Whether you have a question or just want to say hi, I'll try my best to get back to you! </h5>
                                     <p>Best regards, </p>
                                     <p className='mb-3'>Ecaterina </p>
@@ -26,11 +26,11 @@ Whether you have a question or just want to say hi, I'll try my best to get back
                                 </div>
                                 <div className='form-contact'>
                                     <form action='https://getform.io/f/34becda1-3ed4-4bdd-9aa8-244a981dd8c7' method='post'>
-                                        <h5 className='mb-3'>I really appreciate if you would like to send me a feedback.</h5>
+                                        <h5 className='mb-3' style={{textAlign: 'justify'}}>I really appreciate if you would like to send me a feedback.</h5>
                                         <h5 className='mb-3'>Thank you!</h5>
-                                        <input type="text" id='name' name='name' placeholder='i.e.: name' maxlength='30' pattern="[A-Za-z]" required/>
-                                        <input type="text" id='number' name='number' placeholder='i.e.: +0754673231' maxlength='20' pattern="[0-9]" required />
-                                        <input type="email" id='email' name='email' placeholder='i.e.: helloworld@gmail.com' required/>
+                                        <input type="text" id='name' name='name' placeholder='i.e.: name' pattern="^[a-zA-Z]+$" required/>
+                                        <input type="text" id='number' name='number' placeholder='i.e.: +0754673231' maxlength='20' pattern="^[0-9]+$" required />
+                                        <input type="email" id='email' name='email' placeholder='i.e.: helloworld@gmail.com' pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" required/>
                                         <textarea placeholder='message' rows='3' cols='41'></textarea>
                                         <div class="btn-container"><button id='button-form' type="submit" value='submit'>Submit</button></div>
                                     </form>
